097fb6666dba26892b3e344776c9681c653ed86e
Radko Avi <radko.avi@gmail.com>
2018-05-09 11:24:04 +0300

    split_fasta updates

---
+++ R/install_virfinder.R
@@ -0,0 +1,7 @@
+install.packages("glmnet", dependencies=TRUE)
+install.packages("Rcpp", dependencies=TRUE)
+source("https://bioconductor.org/biocLite.R")
+biocLite("qvalue")
+
+install.packages("VirFinder_1.1.tar.gz", repos = NULL, type = "source")
+
--- R/proov1.R
+++ R/proov1.R
@@ -52,34 +52,11 @@ arrange(predResultUser, qvalue) %>% as_data_frame()
 #nende j2rjetused
 #need uude fasta faili
 
-# ids of interest
+# 10 first ids of interest
 ids <- as.character(predResultUser$name[1:10])
 
-# split fasta to sequences
-insert_newlines_concat <- function(x) {
-  x[1] <- paste0(x[1], "\n")
-  x[length(x)] <- paste0(x[length(x)], "\n")
-  paste(x, collapse = "")
-}
+# load subset fasta function
+source("R/subset-fasta.R")
 
-#' Subset fasta file by sequence ids
-#' @param ids fasta ids without '>', character vector
-#' @param infile path to fasta file, character string
-#' @param outfile path to output fasta file, character string
-#' 
-subset_fasta <- function(ids, infile, outfile) {
-  fa <- readLines(infile)
-  i <- grep(">", fa)
-  seqlength <- diff(c(i, length(fa) + 1))
-  splitvec <- rep(1:length(seqlength), seqlength)
-  fasplit <- split(fa, splitvec)
-  names(fasplit) <- fa[i]
-  fasplit <- lapply(fasplit, insert_newlines_concat)
-  fasta_txt <- paste(fasplit[sub(">", "", names(fasplit)) %in% ids], collapse = "")
-  fileConn <- file(outfile, "w")
-  writeLines(fasta_txt, fileConn)
-  close(fileConn)
-}
-
-subset_fasta(ids, "fasta/assembly_7.fa", "output/assembly_7_ids.fa")
+subset_fasta(ids, "fasta/assembly_7.fa", "output/assembly_7_top10.fa")
 
